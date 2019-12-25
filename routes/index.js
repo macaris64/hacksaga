3eb1323cfc0b1bfe2f6678888f56e946e1f9c023
Mehmet Acar <mehmetacar683@gmail.com>
2019-12-25 22:35:39 +0300

    Template Creation 1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hacksaga' });
+  res.render('front/index', { title: 'hacksaga' });
+});
+
+
+router.get('/hackathon', function(req, res, next) {
+  res.render('front/hackathon', { title: 'hacksaga' });
+});
+
+router.get('/course', function(req, res, next) {
+  res.render('front/course', { title: 'hacksaga' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('front/contact', { title: 'hacksaga' });
 });
 
 module.exports = router;
